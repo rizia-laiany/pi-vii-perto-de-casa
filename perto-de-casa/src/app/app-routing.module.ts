a3c4218ca71f2e703f5f30f9d74d021b9f71e50c
rizia-laiany <rizialaianys@gmail.com>
2025-05-21 22:30:45 -0300

    toolbar

--- perto-de-casa/src/app/app-routing.module.ts
+++ perto-de-casa/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'autenticacao'},
+  {
+    path: 'autenticacao',
+    loadChildren: () => import('./autenticacao/autenticacao.module').then(m => m.AutenticacaoModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- perto-de-casa/src/app/app.module.ts
+++ perto-de-casa/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatIconModule } from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
